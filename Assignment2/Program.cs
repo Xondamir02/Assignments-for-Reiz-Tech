6278f3e55bc68b2b3e9c314384ce6e2ad02dc37f
Xondamir Axmadjonov <89469811+Xondamir02@users.noreply.github.com>
2023-04-18 10:37:29 +0500

    Add files via upload

---
+++ Assignment2/Program.cs
@@ -0,0 +1,70 @@
+﻿using Assignment2;
+
+
+// Creating a nested structure
+
+var structure = new Structure
+{
+    Value = 1,
+    Children = new List<Structure>
+      {
+        new Structure
+        {
+          Value = 2,
+           Children = new List<Structure>
+           {
+             new Structure { Value = 4 },
+             new Structure { Value = 5 },
+               new Structure
+               {
+                 Value = 6,
+                  Children = new List<Structure>
+                  {
+                    new Structure { Value = 8 }
+                  }
+               }
+           }
+        },
+       new Structure
+       {
+        Value = 3,
+         Children = new List<Structure>
+         {
+          new Structure { Value = 7 }
+         }
+       }
+    }
+};
+
+
+// Calculating the depth of the structure using recursion
+
+
+int depth = CalculateDepth(structure);
+
+Console.WriteLine("Depth of structure: " + depth);
+
+
+int CalculateDepth(Structure structure)
+{
+    // If the structure has no children, its depth will be 1
+
+    if (structure.Children == null || structure.Children.Count == 0)
+    {
+        return 1;
+    }
+
+    // Recursively calculating the depth of each child and returning the maximum depth
+
+    int maxChildDepth = 0;
+    foreach (var child in structure.Children)
+    {
+        int childDepth = CalculateDepth(child);
+        if (childDepth > maxChildDepth)
+        {
+            maxChildDepth = childDepth;
+        }
+    }
+
+    return maxChildDepth + 1;
+}
---
+++ Assignment2/Structure.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Assignment2
+{
+    public class Structure
+    {
+        public int Value { get; set; }
+        public List<Structure> Children { get; set; }
+
+    }
+}
